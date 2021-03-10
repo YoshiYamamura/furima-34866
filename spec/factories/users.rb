ef4969074018347574c30c5c75e8c493fa0bde15
YoshiYamamura <y.yamamura86@gmail.com>
2021-03-10 19:06:42 +0900

    userモデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Internet.username}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birth                 {Faker::Date.between(from: 80.years.ago, to: Date.today)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+  end
+end
