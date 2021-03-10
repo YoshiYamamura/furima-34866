3f603b0bc7750b1ba66053ced0d67707eb745d95
YoshiYamamura <y.yamamura86@gmail.com>
2021-03-10 16:48:21 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  alphabet_number_mix = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  full_width_japanese = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  full_width_katakana = /\A[ァ-ヶ]+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: full_width_japanese }
+    validates :first_name, format: { with: full_width_japanese }
+    validates :family_name_kana, format: { with: full_width_katakana }
+    validates :first_name_kana, format: { with: full_width_katakana }
+    validates :birth
+    validates :password, format: { with: alphabet_number_mix }
+  end
 end
