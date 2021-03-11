01fc15867c9d7608ac805ec8ac20f6dace477fb6
YoshiYamamura <y.yamamura86@gmail.com>
2021-03-11 12:32:22 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,18 @@ class Item < ApplicationRecord
   belongs_to :fee_allocation
   belongs_to :prefecture
   belongs_to :delivery_days
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :fee_allocation_id
+      validates :prefecture_id
+      validates :delivery_days_id
+    end
+  end
 end
