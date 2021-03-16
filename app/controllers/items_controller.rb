77697d5235fadd72b4833f21f7e45157a8239d9d
YoshiYamamura <y.yamamura86@gmail.com>
2021-03-16 09:36:31 +0900

    購入済み商品のリダイレクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :identificate_user, only: [:edit, :update, :destroy]
-  before_action :soldout_item, except: [:index, :show]
+  before_action :soldout_item, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: 'DESC')
