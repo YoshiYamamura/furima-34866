87dcfc886a454d0a80d090317efc9a9a8bd79771
YoshiYamamura <y.yamamura86@gmail.com>
2021-03-21 19:00:50 +0900

    商品一覧・詳細・編集機能の結合テスト

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -73,3 +73,120 @@ RSpec.describe '商品出品機能', type: :system do
     end
   end  
 end
+
+RSpec.describe '商品一覧・詳細機能', type: :system do
+  before do
+    @item = FactoryBot.create(:item)
+  end
+  context '正常系' do
+    it '出品されている商品の一覧・詳細ページが表示される' do
+      # トップページへ移動する
+      visit root_path
+      # 出品されている商品の商品名、販売価格、配送料の負担、画像が一覧表示されていることを確認する
+      expect(page).to have_content(@item.name)
+      expect(page).to have_content(@item.price.to_s)
+      expect(page).to have_content(@item.fee_allocation.name)
+      expect(page).to have_selector('.item-img')
+      # 商品詳細ページへ移動する
+      visit item_path(@item)
+      # 出品している商品の商品名、商品の説明、販売価格、出品者名、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送日の目安、画像があることを確認する
+      expect(page).to have_content(@item.name)
+      expect(page).to have_content(@item.info)
+      expect(page).to have_content(@item.price.to_s(:delimited))
+      expect(page).to have_content(@item.user.nickname)
+      expect(page).to have_content(@item.category.name)
+      expect(page).to have_content(@item.condition.name)
+      expect(page).to have_content(@item.fee_allocation.name)
+      expect(page).to have_content(@item.prefecture.name)
+      expect(page).to have_content(@item.delivery_period.name)
+      expect(page).to have_selector('.item-box-img')
+    end
+  end
+end
+
+RSpec.describe '商品編集機能', type: :system do
+  before do
+    @item = FactoryBot.create(:item)
+    @edit_item = FactoryBot.build(:item)
+  end
+  context '正常系' do
+    it 'ログインしている出品者が正しい情報を入力すると、商品の編集が出来る' do
+      # ログインする
+      sign_in(@item.user)
+      # 商品詳細ページへ移動する
+      visit item_path(@item)
+      # 商品の編集ボタンがあることを確認する
+      expect(page).to have_content('商品の編集')
+      # 商品編集ページへ移動する
+      visit edit_item_path(@item)
+      # 商品情報を変更入力する
+      attach_file('item[image]', 'public/images/test_image.jpeg')
+      fill_in 'item-name', with: @edit_item.name
+      fill_in 'item-info', with: @edit_item.info
+      select(@edit_item.category.name, from: 'item-category')
+      select(@edit_item.condition.name, from: 'item-sales-status')
+      select(@edit_item.fee_allocation.name, from: 'item-shipping-fee-status')
+      select(@edit_item.prefecture.name, from: 'item-prefecture')
+      select(@edit_item.delivery_period.name, from: 'item-scheduled-delivery')
+      fill_in 'item-price', with: @edit_item.price
+      # 変更するボタンを押してもItemモデルのカウントが上がらないことを確認する
+      expect{
+        click_on('変更する')
+      }.to change { Item.count }.by(0)
+      # 商品詳細ページへ遷移したことを確認する
+      expect(current_path).to eq(item_path(@item))
+      # 出品している商品の商品名、商品の説明、販売価格、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送日の目安、画像が変更されたことを確認する
+      expect(page).to have_content(@edit_item.name)
+      expect(page).to have_content(@edit_item.info)
+      expect(page).to have_content(@edit_item.price.to_s(:delimited))
+      expect(page).to have_content(@edit_item.category.name)
+      expect(page).to have_content(@edit_item.condition.name)
+      expect(page).to have_content(@edit_item.fee_allocation.name)
+      expect(page).to have_content(@edit_item.prefecture.name)
+      expect(page).to have_content(@edit_item.delivery_period.name)
+      expect(page).to have_selector('.item-box-img')
+    end
+  end
+  context '異常系' do
+    it 'ログインしている出品者が誤った情報を入力すると、商品の編集が出来ず商品編集ページに戻る' do
+      # ログインする
+      sign_in(@item.user)
+      # 商品詳細ページへ移動する
+      visit item_path(@item)
+      # 商品の編集ボタンがあることを確認する
+      expect(page).to have_content('商品の編集')
+      # 商品編集ページへ移動する
+      visit edit_item_path(@item)
+      # 商品情報を変更入力する（空欄）
+      fill_in 'item-name', with: ''
+      fill_in 'item-info', with: ''
+      select('--', from: 'item-category')
+      select('--', from: 'item-sales-status')
+      select('--', from: 'item-shipping-fee-status')
+      select('--', from: 'item-prefecture')
+      select('--', from: 'item-scheduled-delivery')
+      fill_in 'item-price', with: ''
+      # 変更するボタンを押す
+      click_on('変更する')
+      # 商品編集ページへ戻されることを確認する
+      expect(current_path).to eq("/items/#{@item.id}")
+      # エラーメッセージが表示されることを確認する
+      expect(page).to have_content('商品名を入力してください')
+    end
+    it 'ログインしている出品者以外のユーザーは、商品編集ページに遷移出来ずトップページにリダイレクトされる' do
+      # ログインする
+      another_user = FactoryBot.create(:user)
+      sign_in(another_user)
+      # 商品編集ページへ移動しようとする
+      visit edit_item_path(@item)
+      # トップページへリダイレクトされることを確認する
+      expect(current_path).to eq(root_path)
+    end
+    it 'ログインしていないユーザーは、商品編集ページに遷移出来ずログインページにリダイレクトされる' do
+      # 商品編集ページへ移動しようとする
+      visit edit_item_path(@item)
+      # ログインページへリダイレクトされることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
